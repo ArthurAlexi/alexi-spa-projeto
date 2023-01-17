86592eefd6adadc156ef0ba7794cbcee542db68c
ArthurAlexi <aammenezes@sga.pucminas.br>
2023-01-17 15:19:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const body = document.body;
+const btn = document.querySelector("#btn");
+const preferenceQuery = window.matchMedia("(prefers-color-scheme: light)");
+
+const addDarkMode = () => {
+  body.classList.remove("light-mode");
+  body.classList.add("dark-mode");
+};
+
+const addLightMode = () => {
+  body.classList.remove("dark-mode");
+  body.classList.add("light-mode");
+};
+
+const toggleTheme = () =>
+  !body.classList.contains("dark-mode") ? addDarkMode() : addLightMode();
+
+const checkPreference = () =>
+  preferenceQuery.matches ? addDarkMode() : addLightMode();
+
+btn.addEventListener("click", toggleTheme);
+preferenceQuery.addEventListener("change", checkPreference);
+// window.addEventListener("DOMContentLoaded", checkPreference);
+(() => checkPreference())();
